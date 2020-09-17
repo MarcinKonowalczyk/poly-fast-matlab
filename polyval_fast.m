3548d264deb0b9cea8949555c8b592f065f8fe94
Christiane Timmel's Group <crtgroup@chem.ox.ac.uk>
2020-09-17 16:58:54 +0100

    Added Vandermode input to polyval

--- polyval_fast.m
+++ polyval_fast.m
@@ -5,20 +5,26 @@ function y = polyval_fast(p,x)
 % Written by Marcin Konowalczyk
 % Timmel Group @ Oxford University
 
-iscolumn_x = iscolumn(x);
-if ~iscolumn_x, x = x'; end % Make x a column vector
-
 p = p(:);
-n = numel(p)-1;
 
-% Construct Vandermonde matrix
-V = zeros(length(x),n+1,'like',x);
-%V(:,n+1) = ones(length(x),1,class(x));
-V(:,n+1) = 1;
-for j = n:-1:1
-   V(:,j) = x.*V(:,j+1);
+if isvector(x)
+    iscolumn_x = iscolumn(x);
+    if ~iscolumn_x, x = x'; end % Make x a column vector
+
+    n = numel(p)-1;
+
+    % Construct Vandermonde matrix
+    V = zeros(length(x),n+1,'like',x);
+    %V(:,n+1) = ones(length(x),1,class(x));
+    V(:,n+1) = 1;
+    for j = n:-1:1
+       V(:,j) = x.*V(:,j+1);
+    end
+
+    y = V*p; % Evaluate polynomial
+    if ~iscolumn_x, y = y'; end % Ensure the output is a column
+else
+    % Assume x *is* the Vandermode matrix
+    y = x*p; % Evaluate polynomial
 end
-
-y = V*p; % Evaluate polynomial
-if ~iscolumn_x, y = y'; end; % Ensure the output is a column
 end
